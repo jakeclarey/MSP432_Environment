4750b574cfec03db6139e7eecf2ef247b31bd266
Jake <jakeclarey@gmail.com>
2025-05-16 14:27:29 -0400

    first commit

---
+++ Blinky/Src/main.c
@@ -0,0 +1,32 @@
+/**
+ * Author: Jacob Clarey
+ * Instructor: Dr. Parikh
+ * Date: 5/15/25
+ * Description: This is the example code from Lab 2. It sets P1 to outputs
+ * and toggles them on and off with a delay in between.
+ * Notes:
+ */
+
+#include "msp.h"
+#include "driverlib.h"
+
+/* Removes error squiggles that are false errors from include path settings */
+#include "msp432p401r.h"
+
+int main(void) {
+  volatile uint32_t i = 0;
+  MAP_WDT_A_holdTimer(); /* Stop watchdog timer */
+
+  P1->SEL0 = 0;
+  P1->SEL1 = 0;
+
+  P1->DIR = 255;
+
+  while (1) {
+    i++;
+    GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    SysCtlDelay(1000000);
+    GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    SysCtlDelay(1000000);
+  }
+}
